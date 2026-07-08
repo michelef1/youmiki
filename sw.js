484283c898ca3d41978073c78d75805a4565b627
michelef1 <michelef654321@gmail.com>
2026-07-08 20:24:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'youmiki-cache-v3';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://img.icons8.com/fluent/512/playlist.png'
+];
+
+// Installazione del Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Attivazione e pulizia vecchia cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Strategia Network-First con fallback su Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
